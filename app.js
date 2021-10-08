43189e7dc5f4ec21a51670930de155ca389e42ae
Felipe Prado <jfpc11@misena.edu.co>
2021-10-07 20:42:59 -0500

    starting an api and deployment to EC2

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+app.get("/", (req, res) => {
+  res.send("<h1> Express DEMO SGMF</ h1>");
+});
+
+app.get("/api/v1", (req, res) => {
+  res.send({
+    status: true,
+  });
+});
+
+app.listen(port, () => {
+  console.log("is runing in port 3000");
+});
