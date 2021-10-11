a823f6515e1d3750d19bfb178209ba0c8fdc2808
Felipe Prado <jfpc11@misena.edu.co>
2021-10-11 15:44:25 -0500

    adding a style code for the project

--- app.js
+++ app.js
@@ -1,16 +1,18 @@
-const express = require("express");
+const express = require('express');
+
 const app = express();
 const port = 3000;
-app.get("/", (req, res) => {
-  res.send("<h1> Express DEMO SGMF</ h1>");
+
+app.get('/', (req, res) => {
+  res.send('<h1> Express DEMO SGMF v0.2 </ h1>');
 });
 
-app.get("/api/v1", (req, res) => {
+app.get('/api/v1', (req, res) => {
   res.send({
     status: true,
   });
 });
 
 app.listen(port, () => {
-  console.log("is runing in port 3000");
+  console.log('is runing in port 3000');
 });
