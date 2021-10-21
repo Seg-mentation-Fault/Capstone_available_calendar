0dcca7e115ec001f1548762c4e4cf05d22dc4a92
Felipe Prado <jfpc11@misena.edu.co>
2021-10-21 14:30:47 -0500

    test for user getUSer

--- tests/testControlllers/user.test.js
+++ tests/testControlllers/user.test.js
@@ -1,5 +1,5 @@
 const storage = require('../../src/connection');
-const { createUser } = require('../../src/controllers/userController');
+const { createUser, getUser } = require('../../src/controllers/userController');
 
 describe('user model', () => {
   afterEach(() => {
@@ -17,5 +17,25 @@ describe('user model', () => {
     expect(actual.firstName).toEqual('user1');
     expect(actual.lastName).toEqual('user1');
     expect(actual.email).toEqual('username@example.com');
+  });
+});
+
+describe('get User', () => {
+  it('Shoud get an user a return it', async () => {
+    const newUser = await createUser(storage, {
+      firstName: 'user1',
+      lastName: 'user1',
+      email: 'username2@example.com',
+    });
+    const findUser = await getUser(storage, { email: 'username2@example.com' });
+    expect(findUser.email).toEqual(newUser.email);
+    expect(findUser.email).toEqual('username2@example.com');
+    await storage.user.destroy({ where: { email: 'username2@example.com' } });
+  });
+
+  it('Shoudl fail and do not get the user', async () => {
+    await expect(
+      getUser(storage, { email: 'username2@example.com' })
+    ).rejects.toBeInstanceOf(Error);
   });
 });
