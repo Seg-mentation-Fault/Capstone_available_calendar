9b97b279984fea61cc955d863868d583a912aae7
Louvani <paula.louvani@gmail.com>
2021-10-26 20:29:59 -0500

    Add method getAllParksById, in newParkCapacity  & updateParkCapacity move logic of full capacity for the service.

--- src/service/updateParkCapacity.js
+++ src/service/updateParkCapacity.js
@@ -5,9 +5,9 @@ const { updateParkCapacity } = require('../controllers/parkCapacityController');
  * @async
  * @param {} storage - Constructor of the data base strorage.
  * @param {Object} data - Object with data to find parks
- * @param {String} data.ParkId -number of the park id.
+ * @param {Number} data.ParkId -number of the park id.
  * @param {String} data.date - String with the date farmat "yyyy-mm-dd"
- * @param {String} data.parkCapacity - the new capacity to update.
+ * @param {Number} data.dayCapacity - the new capacity to update.
  * @return {Array} Array - a list of parks capacities for a given park.
  */
 const updateCapacity = async (storage, data) => {
@@ -18,8 +18,12 @@ const updateCapacity = async (storage, data) => {
     const fullCapacity = await storage.park.findOne({
       attributes: ['capacity'],
       where: { id: data.ParkId },
+      raw: true,
     });
-    if (fullCapacity < data.parkCapacity) {
+    if (fullCapacity === null) {
+      throw new Error('Park ID does not exist');
+    }
+    if (fullCapacity.capacity < data.dayCapacity) {
       throw new Error(
         'the new capacity cannot be greater than the full capacity of the park'
       );
@@ -28,7 +32,8 @@ const updateCapacity = async (storage, data) => {
     const updated = await updateParkCapacity(
       storage,
       { ParkId: data.ParkId, date: data.date },
-      { parkCapacity: data.parkCapacity }
+      { dayCapacity: data.dayCapacity },
+      t
     );
     await t.commit();
     return updated;
