374f5efaeae7281e904662dacf9cdcce00702749
Louvani <paula.louvani@gmail.com>
2021-10-26 20:30:54 -0500

    Routes Routes to post new capacity and update it

--- src/routes/parkCapacity.js
+++ src/routes/parkCapacity.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const { body, validationResult } = require('express-validator');
 const { parksByDay } = require('../service/parksByDay');
+const { parksById } = require('../service/parksCapacityById');
+const { updateCapacity } = require('../service/updateParkCapacity');
+const { addParkCapacity } = require('../service/addParkCapacity');
 
 const router = express.Router();
 
@@ -14,8 +17,27 @@ const validation = [
     .isDate()
     .withMessage('date should an actual date format yyyy-mm-dd'),
 ];
+const validation2 = [
+  body('ParkId')
+    .trim()
+    .escape()
+    .isInt()
+    .withMessage('ParkI ID should be an integer'),
+];
+const validation3 = [
+  body('date')
+    .isDate()
+    .withMessage('date should an actual date format yyyy-mm-dd'),
+  body('dayCapacity')
+    .trim()
+    .escape()
+    .isInt()
+    .withMessage('park capacity should be an Integer'),
+  body('ParkId').isInt().withMessage('ParkI ID should be an integer'),
+];
 
 module.exports = (storage) => {
+  // get all parks with availability status for n guests
   router.post('/', validation, async (req, res) => {
     try {
       const errors = validationResult(req);
@@ -30,5 +52,57 @@ module.exports = (storage) => {
     }
   });
 
+  // get all dates with thei capacity for a given park
+  router.post('/byparkid', validation2, async (req, res) => {
+    try {
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+      }
+      const { ParkId } = req.body;
+      const parks = await parksById(storage, { ParkId });
+      return res.json(parks);
+    } catch (err) {
+      return res.status(400).json({ error: err.message });
+    }
+  });
+
+  // Add a new park capacity for a park on a given day
+  router.post('/add', validation3, async (req, res) => {
+    try {
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+      }
+      const { ParkId, date, dayCapacity } = req.body;
+      const parks = await addParkCapacity(storage, {
+        ParkId,
+        date,
+        dayCapacity,
+      });
+      return res.json(parks);
+    } catch (err) {
+      return res.status(400).json({ error: err.message });
+    }
+  });
+
+  // update a par capacity for a given day and park ID
+  router.put('/update', validation3, async (req, res) => {
+    try {
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+      }
+      const { ParkId, date, dayCapacity } = req.body;
+      const parks = await updateCapacity(storage, {
+        ParkId,
+        date,
+        dayCapacity,
+      });
+      return res.json(parks);
+    } catch (err) {
+      return res.status(400).json({ error: err.message });
+    }
+  });
   return router;
 };
---
+++ src/service/addParkCapacity.js
@@ -0,0 +1,49 @@
+const { newParkCapacity } = require('../controllers/parkCapacityController');
+
+/**
+ * updateCapacity - Update the capacity of a park for a given date
+ * @async
+ * @param {} storage - Constructor of the data base strorage.
+ * @param {Object} data - Object with data to find parks
+ * @param {Number} data.ParkId -number of the park id.
+ * @param {String} data.date - String with the date farmat "yyyy-mm-dd"
+ * @param {Number} data.dayCapacity - the new capacity to update.
+ * @return {Array} Array - a list of parks capacities for a given park.
+ */
+const addParkCapacity = async (storage, data) => {
+  const t = await storage.client.transaction();
+
+  try {
+    // get the full capacity of a park
+    const fullCapacity = await storage.park.findOne({
+      attributes: ['capacity'],
+      where: { id: data.ParkId },
+      raw: true,
+    });
+    if (fullCapacity === null) {
+      throw new Error('Park ID does not exist');
+    }
+    if (fullCapacity.capacity < data.dayCapacity) {
+      throw new Error(
+        'the new capacity cannot be greater than the full capacity of the park'
+      );
+    }
+    // Update the park capacity for the given date
+    const newCapacity = await newParkCapacity(
+      storage,
+      {
+        ParkId: data.ParkId,
+        date: data.date,
+        dayCapacity: data.dayCapacity,
+      },
+      t
+    );
+    await t.commit();
+    return newCapacity;
+  } catch (err) {
+    await t.rollback();
+    throw err;
+  }
+};
+
+exports.addParkCapacity = addParkCapacity;
